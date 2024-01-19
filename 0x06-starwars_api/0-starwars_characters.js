c9d103c65c28f20757b4eecfee3685d8c99352c3
unkown007 <victormatusse00@gmail.com>
2024-01-19 14:56:54 +0200

    added file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,13 +9,13 @@ request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`, (error,
   const characters = JSON.parse(body).characters;
   const charactersName = characters.map(
     url => new Promise((resolve, reject) => {
-    request(url, (error1, response1, body1) => {
-      if (error1) {
-        reject(error1);
-      }
-      resolve(JSON.parse(body1).name);
-    });
-  }));
+      request(url, (error1, response1, body1) => {
+        if (error1) {
+          reject(error1);
+        }
+        resolve(JSON.parse(body1).name);
+      });
+    }));
 
   Promise.all(charactersName)
     .then(names => console.log(names.join('\n')))
